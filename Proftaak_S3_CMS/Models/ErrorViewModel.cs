1c939c4865c6f2a781bc4a2d0125f1fb51853026
davey2206 <davey2206@live.nl>
2022-09-29 13:00:05 +0200

    Add project files.

---
+++ Proftaak_S3_CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proftaak_S3_CMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
